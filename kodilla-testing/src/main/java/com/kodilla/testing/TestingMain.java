86920df5cd373d54f9b83a6460183e95f1469e27
krman1 <mar.aniol@interia.pl>
2019-01-21 23:28:03 +0100

    First Test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        int firstNumber = 4;
+        int secoundNumber = 2;
+        int add = firstNumber + secoundNumber;
+        int subtract = firstNumber - secoundNumber;
+
+        int results = calculator.add(4, 2);
+
+        if (add == results) {
+            System.out.println("Test dodawania - OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        results = calculator.subtract(4, 2);
+
+        if (subtract == results) {
+            System.out.println("Test odejmowania - OK");
+        } else {
+            System.out.println("Error!");
+
+        }
     }
 }
