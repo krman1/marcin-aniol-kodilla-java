33aaea3be71729a87f5e12c004e0b45e91959a99
krman1 <mar.aniol@interia.pl>
2019-02-27 21:51:31 +0100

    Portfolio

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -17,7 +17,7 @@ System.out.println("Liczby dla których będzie obliczana średnia:");
 
         return IntStream.range(0,numbers.length)
                 .map(n -> numbers[n])
-                .average().getAsDouble();
+            .average().getAsDouble();
 
     }
 }
