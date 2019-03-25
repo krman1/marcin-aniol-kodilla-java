30e1cb839ea54959ef62dbfe2045a36e2ee0ead5
krman1 <mar.aniol@interia.pl>
2019-03-25 23:27:28 +0100

    Tablica z listami zadań

---
+++ com.kodilla.spring1/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,36 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.util.List;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoardTestSuite {
+
+    @Test
+    public void testTaskAdd() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = context.getBean(Board.class);
+        //When
+        List<String> toDoList = board.getToDoList().getTasks();
+        List<String> doneList = board.getDoneList().getTasks();
+        List<String> inProgressList = board.getInProgressList().getTasks();
+        doneList.add("Projekt - Smidowicza");
+        inProgressList.add("Projket - Dymka");
+        toDoList.add("Projket - Jar IV");
+        //Then
+        Assert.assertEquals("Projekt - Smidowicza",toDoList.get(0));
+        Assert.assertEquals("Projket - Dymka",inProgressList.get(0));
+        Assert.assertEquals("Projket - Jar IV",doneList.get(0));
+
+    }
+}
+
