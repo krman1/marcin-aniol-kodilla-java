e613904db22150dd0c3cc712254855b5fdaef3d0
krman1 <mar.aniol@interia.pl>
2019-03-26 23:34:53 +0100

    Spring Web

--- com.kodilla.spring1/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ com.kodilla.spring1/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.util.Arrays;
 
 @RunWith(SpringRunner.class)
